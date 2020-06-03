fa0151d0a25f2809705cc12e8affce0552b69aa8
Gao Wang <gwan58@student.monash.edu>
2020-06-03 16:59:39 +0800

    Re-use existing authentication resource like UserPool and Identity pool

--- fit5225Amplify/src/app.js
+++ fit5225Amplify/src/app.js
@@ -1,3 +1,60 @@
 import Amplify, { Auth } from 'aws-amplify';
-import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
+
+Amplify.configure({
+    Auth: {
+
+        // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+        identityPoolId: 'us-east-1:12d3b977-42b4-4dd6-83d4-b8cf227128c4',
+
+        // REQUIRED - Amazon Cognito Region
+        region: 'us-east-1',
+
+        // OPTIONAL - Amazon Cognito Federated Identity Pool Region
+        // Required only if it's different from Amazon Cognito Region
+        identityPoolRegion: 'us-east-1',
+
+        // OPTIONAL - Amazon Cognito User Pool ID
+        userPoolId: 'us-east-1_FAswJTHLh',
+
+        // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+        userPoolWebClientId: '7o0nh2h9gltje27ca87nc5m8in',
+
+        // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+        mandatorySignIn: true,
+
+        // // OPTIONAL - Configuration for cookie storage
+        // // Note: if the secure flag is set to true, then the cookie transmission requires a secure protocol
+        // cookieStorage: {
+        //     // REQUIRED - Cookie domain (only required if cookieStorage is provided)
+        //     domain: '.yourdomain.com',
+        //     // OPTIONAL - Cookie path
+        //     path: '/',
+        //     // OPTIONAL - Cookie expiration in days
+        //     expires: 365,
+        //     // OPTIONAL - Cookie secure flag
+        //     // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
+        //     secure: true
+        // },
+        //
+        // // OPTIONAL - customized storage object
+        // storage: new MyStorage(),
+        //
+        // // OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
+        // authenticationFlowType: 'USER_PASSWORD_AUTH',
+        //
+        // // OPTIONAL - Manually set key value pairs that can be passed to Cognito Lambda Triggers
+        // clientMetadata: { myCustomKey: 'myCustomValue' },
+        //
+        // // OPTIONAL - Hosted UI configuration
+        // oauth: {
+        //     domain: 'your_cognito_domain',
+        //     scope: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
+        //     redirectSignIn: 'http://localhost:3000/',
+        //     redirectSignOut: 'http://localhost:3000/',
+        //     responseType: 'code' // or 'token', note that REFRESH token will only be generated when the responseType is code
+        // }
+    }
+});
+
+// You can get the current config object
+const currentConfig = Auth.configure();
