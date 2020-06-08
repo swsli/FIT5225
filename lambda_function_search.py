ff6a7efab1640a40ed07449047b2874738b781f7
Gao Wang <gwan58@student.monash.edu>
2020-06-08 19:37:55 +0800

    update search

--- lambda_function_search.py
+++ lambda_function_search.py
@@ -5,8 +5,8 @@ import boto3
 def searchImageByTag(tags, dynamodb=None):
     if not dynamodb:
         dynamodb = boto3.resource("dynamodb")
-    table = dynamodb.Table("asgn2-dynamodb")
-    
+    table = dynamodb.Table("fit5225dynamodb")
+
     j = 1
     filterExpression = ""
     expressionAttrVal = {}
@@ -17,30 +17,38 @@ def searchImageByTag(tags, dynamodb=None):
             filterExpression = filterExpression + " AND " + "contains(tags, :tag"+str(j)+")"
         expressionAttrVal[':tag'+ str(j)] = i
         j += 1
-    
+
     response = table.scan(
-        ProjectionExpression= "imageUrl",
+        ProjectionExpression= "link",
         FilterExpression= filterExpression,
         ExpressionAttributeValues= expressionAttrVal)
-  
-    
+    print(filterExpression)
+    print(expressionAttrVal)
+    print("Response:")
+    print(response)
     result = []
     items = response['Items']
-    
+
     for i in items:
-        result.append(i['imageUrl'])
+        result.append(i['link'])
     return {"links": result}
-    
+
 
 
 def lambda_handler(event, context):
     # TODO implement
-    tags = []
-    tags = json.loads(event['body'])['Tags']
-    
+    tags = event['queryStringParameters']
+    tagValue = []
+    for tag in tags:
+        tagValue.append(tags[tag])
+
+
 
     return {
         'statusCode': 200,
-        'body': json.dumps(searchImageByTag(tags))
+        'headers': {
+            "Access-Control-Allow-Origin" : "*",
+            "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, x-amz-security-token, authorization,  x-amz-date"
+        },
+        'body': json.dumps(searchImageByTag(tagValue))
     }
-
